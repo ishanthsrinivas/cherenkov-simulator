60e55dafdd878118c64992e46e7e80243bbfaf60
ishanthsrinivas <75952441+ishanthsrinivas@users.noreply.github.com>
2024-10-15 18:59:40 -0500

    Add files via upload

---
+++ detector.cc
@@ -0,0 +1,21 @@
+#include "detector.hh"
+
+MySensitiveDetector::MySensitiveDetector(G4String name) : G4VSensitiveDetector(name){}
+
+MySensitiveDetector::~MySensitiveDetector(){}
+
+G4bool MySensitiveDetector::ProcessHits(G4Step *aStep, G4TouchableHistory *ROhist){
+
+	G4Track *track = aStep->GetTrack();
+	track->SetTrackStatus(fStopAndKill);
+	
+	G4StepPoint *preStepPoint = aStep->GetPreStepPoint();
+	G4StepPoint *postStepPoint = aStep->GetPostStepPoint();
+	
+	G4ThreeVector posPhoton = preStepPoint->GetPosition();
+	const G4VTouchable *touchable = aStep->GetPreStepPoint()->GetTouchable();
+	
+	G4int copyNo = touchable->GetCopyNumber();
+	G4cout << "copy number:" << copyNo << G4endl;
+
+}
